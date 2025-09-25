1443b6cf822b433a1102792c1f471698c8c09086
developrershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-09-25 21:34:08 +0600

    addad the new line

--- leepYear.js
+++ leepYear.js
@@ -7,4 +7,18 @@ function isLeepYear (year){
     }
 }
 const isLipi = isLeepYear(2024);
-console.log(isLipi);
+// console.log(isLipi);
+
+function isLeepYear2 (year){
+    if(year % 100 !== 0 && year % 4 === 0){
+        return true;
+    }
+    else if(year % 100 === 0 &&  year % 400 === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+const isLeap = isLeepYear2(2100);
+console.log(isLeap);
