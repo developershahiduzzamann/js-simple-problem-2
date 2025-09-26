6dace41825695153634b6046ad5feafd86c686a7
developrershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-09-26 19:22:47 +0600

    addad the avg number

--- odd-average.js
+++ odd-average.js
@@ -1,9 +1,21 @@
 function oddAverage(numbers){
+    const odds = [];
     for(const number of numbers){
         if(number % 2 === 1){
-            console.log(number);
+            // console.log(number);
+            odds.push(number);
         }
     }
+    // console.log(odds);
+    let sum = 0;
+    for(const number of odds){
+        sum = sum + number;
+    }
+    const count = odds.length;
+    console.log(sum, count);
+    const avg = sum / count;
+    return avg
+
 }
 const numbers = [11, 34, 65, 66, 33, 62, 71];
 const avg =  oddAverage(numbers);
