6dab2173007ca20f82cbe3951c0c88ed4b5c4904
developrershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-09-27 06:04:21 +0600

    addad the new line

--- no-duplicate.js
+++ no-duplicate.js
@@ -2,7 +2,13 @@ const biryanikhor = ['sabbir', 'topu', 'babu', 'tarik', 'sabbir', 'topu', 'tarik
 const numbers = [11, 24, 56, 77, 11, 24, 25, 77, 56];
 
 function noDuplicate (array){
-    console.log(array)
+    const unique = [];
+    for(const item of array){
+        if(unique.includes(item) === false){
+            unique.push(item);
+        }
+    }
+    return unique;
 }
 const uniquueArray =noDuplicate(biryanikhor);
 console.log(uniquueArray);
