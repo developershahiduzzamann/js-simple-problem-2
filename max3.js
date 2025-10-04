a858f3fd9e6b64d38b8a82aeea2874883f8e047a
developrershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-10-04 06:49:16 +0600

    addad the max3 file

---
+++ max3.js
@@ -0,0 +1,13 @@
+const jim = 68;
+const tim = 56;
+const kim = 89;
+
+if(jim > tim && jim > kim){
+    console.log('jis is the boss');
+}
+else if (tim > jim && tim > kim){
+    console.log('tim is the boss two');
+}
+else{
+    console.log('kim is the boss 3');
+}
